257ae03425d64a5dc7b458d004068f9f77e3bd89
wanto-production <ikwansatria3974@gmail.com>
2025-02-26 15:12:13 +0700

    initial commit

---
+++ controller/message.controller.ts
@@ -0,0 +1,49 @@
+import type { Context } from "grammy";
+import { InputMediaBuilder } from "grammy";
+
+export class MessageController {
+
+    static async main(c: Context) {
+        if (
+            c.message?.text &&
+            !RegExp('https?:\/\/(vt|vn|vm)\.tiktok\.com\/[a-zA-Z0-9]+').test(c.message.text)
+        ) return c.reply("Oops wrong format!");
+
+        const message = await c.reply("wait🕛...")
+
+        const res = await fetch(`https://tiktok-download-without-watermark.p.rapidapi.com/analysis?url=${c.message?.text}&hd=0`, {
+            headers: {
+                'x-rapidapi-key': process.env["RAPIDAPI_KEY"] as string,
+                'x-rapidapi-host': 'tiktok-download-without-watermark.p.rapidapi.com'
+            },
+        })
+
+        const data = await res.json()
+
+        console.log(`download completed from: ${c.from?.username}`)
+
+        if (data.code < 0) {
+            return await Promise.all([
+                c.reply(`Oops ${data.msg}`),
+                c.api.deleteMessage(c.chatId as number, message.message_id)
+            ])
+        }
+
+        if (data.data.images) {
+            return await Promise.all([
+                c.replyWithMediaGroup(data.data.images.map((image: string) => InputMediaBuilder.photo(image))),
+                c.reply("completed! ✅, type:photo"),
+                c.api.deleteMessage(c.chatId as number, message.message_id),
+            ])
+        }
+
+        if ((data.data.play as string).includes("mp4")) {
+            return await Promise.all([
+                c.api.sendVideo(c.chatId as number, data.data.play, { caption: "completed! ✅, type:video" }),
+                c.api.deleteMessage(c.chatId as number, message.message_id),
+            ])
+        }
+
+    }
+
+}
--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -1,4 +1,5 @@
-import { Bot, webhookCallback, InputMediaBuilder } from "grammy";
+import { Bot, webhookCallback } from "grammy";
+import { MessageController } from "@/controller/message.controller";
 
 const bot = new Bot(process.env["TELEGRAM_TOKEN"] as string)
 
@@ -10,47 +11,7 @@ bot.command("start", (c) => {
     c.reply("hello🤖,\ni am a telegram bot\nmade by t.me/iwanSlebew to convert tiktok links to video/photos.\n")
 })
 
-bot.on("message", async (c) => {
-    if (
-        c.message.text &&
-        !RegExp('https?:\/\/(vt|vn|vm)\.tiktok\.com\/[a-zA-Z0-9]+').test(c.message.text)
-    ) return c.reply("Oops wrong format!");
-
-    const message = await c.reply("wait🕛...")
-
-    const res = await fetch(`https://tiktok-download-without-watermark.p.rapidapi.com/analysis?url=${c.message.text}&hd=0`, {
-        headers: {
-            'x-rapidapi-key': process.env["RAPIDAPI_KEY"] as string,
-            'x-rapidapi-host': 'tiktok-download-without-watermark.p.rapidapi.com'
-        },
-    })
-
-    const data = await res.json()
-
-    console.log(`download completed from: ${c.from.username}`)
-
-    if (data.code < 0) {
-        return await Promise.all([
-            c.reply(`Oops ${data.msg}`),
-            c.api.deleteMessage(c.chatId as number, message.message_id)
-        ])
-    }
-
-    if (data.data.images) {
-        return await Promise.all([
-            c.replyWithMediaGroup(data.data.images.map((image: string) => InputMediaBuilder.photo(image))),
-            c.reply("completed! ✅, type:photo"),
-            c.api.deleteMessage(c.chatId as number, message.message_id),
-        ])
-    }
-
-    if ((data.data.play as string).includes("mp4")) {
-        return await Promise.all([
-            c.api.sendVideo(c.chatId as number, data.data.play, { caption: "completed! ✅, type:video" }),
-            c.api.deleteMessage(c.chatId as number, message.message_id),
-        ])
-    }
-})
+bot.on("message", MessageController.main)
 
 bot.catch((error) => {
     console.log(error.message)
