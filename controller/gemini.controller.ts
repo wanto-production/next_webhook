a555b7616d27042254efa39506791602649cfa21
wanto-production <ikwansatria3974@gmail.com>
2025-02-26 19:40:21 +0700

    initial commit

--- controller/gemini.controller.ts
+++ controller/gemini.controller.ts
@@ -5,7 +5,13 @@ import { getSession, saveSession } from "@/utils/database";
 export class GeminiController {
     static async main(ctx: Context) {
         // Pastikan update berisi pesan teks
-        ctx.reply(ctx.update?.message?.text!)
+        ctx.reply(`
+            <pre>
+                ${JSON.stringify(ctx)}
+            </pre>
+        `, {
+            parse_mode: "HTML"
+        })
     }
 }
 
