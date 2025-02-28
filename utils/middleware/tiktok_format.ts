1cc564b35472bf12f7f3037309b74df96f647976
wanto-production <ikwansatria3974@gmail.com>
2025-02-28 14:00:57 +0700

    initial commit

--- utils/middleware/tiktok_format.ts
+++ utils/middleware/tiktok_format.ts
@@ -1,11 +1,10 @@
 import { Context } from "grammy";
 
-export async function tiktokFormatMiddleware(c: Context): Promise<void | never> {
+export async function tiktokFormatMiddleware(c: Context): Promise<any> {
     if (
         c.message?.text &&
         !RegExp('https?:\/\/(vt|vn|vm)\.tiktok\.com\/[a-zA-Z0-9]+').test(c.message.text)
     ) {
-        await c.reply("Oops wrong format!");
-        return;
+        return await c.reply("Oops wrong format!");
     }
 }
