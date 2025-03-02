f4b9cff91be4a8aecf92d8e4fc5c57631d64293f
wanto-production <ikwansatria3974@gmail.com>
2025-03-02 10:14:14 +0700

    initial commit

--- utils/markdown.ts
+++ utils/markdown.ts
@@ -1,4 +1,9 @@
-export function escapeMarkdownV2(text: string) {
-    return text.replace(/[_*[\]()~`>#+\-=|{}.!]/g, "\\$&");
-}
+export function escapeMarkdownV2(text: string): string {
+    // Karakter yang harus di-escape di MarkdownV2 Telegram
+    const specialChars = ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!'];
 
+    // Menggunakan regex untuk menggantikan setiap karakter dengan versi yang di-escape
+    const escapedText = text.replace(/[_*\[\]()~`>#\+\-=|{}.!]/g, '\\$&');
+
+    return escapedText;
+}
