46348572300dc21abc66475d49a00c19f295ffcd
wanto-production <ikwansatria3974@gmail.com>
2025-03-02 10:29:41 +0700

    initial commit

--- utils/markdown.ts
+++ utils/markdown.ts
@@ -1,9 +1,13 @@
 export function escapeMarkdownV2(text: string): string {
     return text
-        .replace(/(\*{3,})/g, "**") // Menghindari *** yang tidak jelas
-        .replace(/(\*{2})([^*]+)\1?/g, "**$2**") // Memastikan **bold** selalu ditutup
-        .replace(/(\*)([^*]+)\1?/g, "*$2*") // Memastikan *italic* selalu ditutup
-        .replace(/(`{2,})/g, "`") // Menghindari ```` yang berlebihan
-        .replace(/(\[)([^\]]+)(\]\()/g, "[$2](") // Menormalkan format link
-        .replace(/\[(.*?)\]\((?!http)(.*?)\)/g, "[$1](https://$2)"); // Auto-perbaiki link tanpa protocol}
+        .replace(/_/g, "\\_") // Escape underscore
+        .replace(/\*/g, "\\*") // Escape asterisk
+        .replace(/\[/g, "\\[") // Escape bracket [
+        .replace(/\]/g, "\\]") // Escape bracket ]
+        .replace(/\(/g, "\\(") // Escape parenthesis (
+        .replace(/\)/g, "\\)") // Escape parenthesis )
+        .replace(/`/g, "\\`") // Escape backtick `
+        .replace(/\./g, "\\.") // Escape dot
+        .replace(/-/g, "\\-") // Escape dash
+        .replace(/#/g, "\\#"); // Escape hash
 }
